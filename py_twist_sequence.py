165ea5d544152dd91a79289de7fc3f387a1dbf00
underknown50-dotcom <underknown50@gmail.com>
2026-05-28 08:37:29 +0300

    Delete py_twist_sequence.py

--- py_twist_sequence.py
+++
@@ -1,9 +0,0 @@
-def twist_sequence(arr: list[int], k: int) -> list[int]:
-    if not arr:
-        return []
-
-    n = len(arr)
-    k = k % n
-    if k == 0:
-        return arr[:]
-    return arr[-k:] + arr[:-k]
