00d0f663db875c7a51f376c55aae6916d564ba54
underknown50-dotcom <underknown50@gmail.com>
2026-05-28 08:38:37 +0300

    Delete py_echo_validator.py

--- py_echo_validator.py
+++
@@ -1,7 +0,0 @@
-def echo_validator(text: str) -> bool:
-    cleaned = ''.join(ch.lower() for ch in text if ch.isalpha())
-
-    if not cleaned:
-        return False
-
-    return cleaned == cleaned[::-1]
